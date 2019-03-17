3c93338cc719b45ea4c2d7b14c6ba3fe3f2e27ed
ravishankarojha <ravi.oj@hcl.com>
2019-03-17 12:08:46 +0530

    Update mtaBuild.groovy

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -47,7 +47,7 @@ void call(Map parameters = [:]) {
             def mta = new JavaArchiveDescriptor('SAP Multitarget Application Archive Builder', 'MTA_JAR_LOCATION', 'mtaJarLocation', '1.0.6', '-v', java)
 
             def mtaYamlName = "mta.yaml"
-            def applicationName = configuration.applicationName
+            def applicationName = HCP
 
             if (!fileExists(mtaYamlName)) {
                 if (!applicationName) {
