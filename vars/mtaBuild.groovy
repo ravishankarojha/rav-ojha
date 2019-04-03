7ade303c43b941790ab1297d7865a0d2e1a822f5
ravishankarojha <ravi.oj@hcl.com>
2019-04-03 16:29:37 +0530

    Update mtaBuild.groovy

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -1,10 +1,9 @@
 import static com.sap.piper.Prerequisites.checkScript
 
+import com.sap.piper.GenerateDocumentation
 import com.sap.piper.ConfigurationHelper
 import com.sap.piper.MtaUtils
 import com.sap.piper.Utils
-import com.sap.piper.tools.JavaArchiveDescriptor
-import com.sap.piper.tools.ToolDescriptor
 
 import groovy.transform.Field
 
@@ -12,16 +11,32 @@ import groovy.transform.Field
 
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STEP_CONFIG_KEYS = [
+    /** The name of the application which is being built. If the parameter has been provided and no `mta.yaml` exists, the `mta.yaml` will be automatically generated using this parameter and the information (`name` and `version`) from `package.json` before the actual build starts.*/
     'applicationName',
+    /**
+     * The target platform to which the mtar can be deployed.
+     * @possibleValues 'CF', 'NEO', 'XSA'
+     */
     'buildTarget',
+    /** @see dockerExecute */
     'dockerImage',
+    /** The path to the extension descriptor file.*/
     'extension',
+    /**
+     * The location of the SAP Multitarget Application Archive Builder jar file, including file name and extension.
+     * If it is not provided, the SAP Multitarget Application Archive Builder is expected on PATH.
+     */
     'mtaJarLocation'
 ]
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([
+    /** @see dockerExecute */
     'dockerOptions'
 ])
 
+/**
+ * Executes the SAP Multitarget Application Archive Builder to create an mtar archive of the application.
+ */
+@GenerateDocumentation
 void call(Map parameters = [:]) {
     handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
 
@@ -43,11 +58,9 @@ void call(Map parameters = [:]) {
         ], configuration)
 
         dockerExecute(script: script, dockerImage: configuration.dockerImage, dockerOptions: configuration.dockerOptions) {
-            def java = new ToolDescriptor('Java', 'JAVA_HOME', '', '/bin/', 'java', '1.8.0', '-version 2>&1')
-            def mta = new JavaArchiveDescriptor('SAP Multitarget Application Archive Builder', 'MTA_JAR_LOCATION', 'mtaJarLocation', '1.0.6', '-v', java)
 
             def mtaYamlName = "mta.yaml"
-            def applicationName =  configuration.applicationName
+            def applicationName = configuration.applicationName
 
             if (!fileExists(mtaYamlName)) {
                 if (!applicationName) {
@@ -61,7 +74,7 @@ void call(Map parameters = [:]) {
                 echo "[INFO] '${mtaYamlName}' file found in project sources."
             }
 
-            def mtaYaml = readYaml file: "/hana/backup/mta_archive_builder.jar"
+            def mtaYaml = readYaml file: mtaYamlName
 
             //[Q]: Why not yaml.dump()? [A]: This reformats the whole file.
             sh "sed -ie \"s/\\\${timestamp}/`date +%Y%m%d%H%M%S`/g\" \"${mtaYamlName}\""
@@ -72,7 +85,9 @@ void call(Map parameters = [:]) {
             }
 
             def mtarFileName = "${id}.mtar"
-            def mtaJar = mta.getCall(this, configuration)
+            // If it is not configured, it is expected on the PATH
+            def mtaJar = 'java -jar '
+            mtaJar += configuration.mtaJarLocation ?: 'mta.jar'
             def buildTarget = configuration.buildTarget
 
             def mtaCall = "${mtaJar} --mtar ${mtarFileName} --build-target=${buildTarget}"
@@ -81,10 +96,9 @@ void call(Map parameters = [:]) {
             mtaCall += ' build'
 
             echo "[INFO] Executing mta build call: '${mtaCall}'."
-            
-            export HOME=/tmp
-
+             
             sh """#!/bin/bash
+            export HOME=/tmp
             export PATH=./node_modules/.bin:${PATH}
             $mtaCall
             """
